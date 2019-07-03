d7ffc4148ec653e05fb3c2e00eec2fec5e324265
Alexander Sobyanin <a.sobyanin@carbonsoft.ru>
2019-07-03 21:59:25 +0500

    Feat: script to move player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private int lastHorizontaMovelDirectionIsLeft = 0;
+    private int lastVerticalMovelDirectionIsUp = 0;
+
+    void Update()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+    }
+
+    public void MoveLeft()
+    {
+
+    }
+
+    public void MoveRight()
+    {
+
+    }
+}
